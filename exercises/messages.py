704c0689570877ea04afa8a67dfc6c240c14e284
Taru Wiik <taruwiik@gmail.com>
2026-07-19 19:19:47 +0300

    Added exercisess 2-1 and 2-2 to messages.py in order to practice how variables represent data in Python programs.

---
+++ exercises/messages.py
@@ -0,0 +1,13 @@
+# 2-1 Simple Message: Assign a message to a variable, and then print that message
+
+message = "This is a simple message"
+print(message)
+
+""" 2-2 Simple Messages: Assign a message to a variable, and print that message. Then change the value of the 
+variable to a new message, and print the new message.  """
+
+message = "This is another simple message"
+print(message)
+
+message = "This is the new simple message" 
+print(message) 
