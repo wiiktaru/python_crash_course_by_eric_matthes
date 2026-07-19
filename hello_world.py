b57b7ddb796422e77f67b5e49135a805a4988320
Taru Wiik <taruwiik@gmail.com>
2026-07-19 16:52:37 +0300

    As a note for myself, added a comment on good naming practices

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,6 @@
 # Introducing variable "message" that is connected to a value "Hello Python world"
+# Variable names: only letters, numbers and underscores. Can't start with a number. Spaces ara not allowed. 
+# Do not use Python keywords or function names. Should be short and descriptive. 
 message = "Hello Python world!"
 
 print(message)
