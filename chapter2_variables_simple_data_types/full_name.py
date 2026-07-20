60f7c9f0658bf881a7a0a0437751a01b0ffbfe59
Taru Wiik <taruwiik@gmail.com>
2026-07-20 08:55:55 +0300

    Added a full_name.py file to chapter2_variables_simple_data_types folder in order to practice string variable. Added code introducing f-string

---
+++ chapter2_variables_simple_data_types/full_name.py
@@ -0,0 +1,9 @@
+# String variables 
+first_name = 'john'
+last_name = 'doe'
+
+""" In f-string Python formats the string by replacing the named variable in braces with its value 
+Letter f is placed before the opening quatation of the string """
+
+full_name = f"{first_name} {last_name}"
+print(full_name)
