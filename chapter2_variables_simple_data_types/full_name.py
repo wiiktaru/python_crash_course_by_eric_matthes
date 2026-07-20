4c94d36ded00a970b1285c0fb5e206fb7fb6e1b7
Taru Wiik <taruwiik@gmail.com>
2026-07-20 19:14:18 +0300

    Added examples of using whitespace to create readability to full_name.py

--- chapter2_variables_simple_data_types/full_name.py
+++ chapter2_variables_simple_data_types/full_name.py
@@ -14,3 +14,16 @@ print(f"Hello {full_name.title()}!")
 # Example of assignin the f-string composed message to a variable. This makes the final print() call more simple
 message = f"Hello {full_name.title()}!"
 print(message)
+
+""" Example of whitespace - any nonprinting characters (space, tab, end-of-line sympols). 
+ Used to organize output for readability  """
+
+# Whitespace using tab 
+print("Python")
+print("\tPython")
+
+# Whitespace using line 
+print("Languages: \nPython\nC\nJavaScript")
+
+# Combining tab and line as a whitespace
+print("Languages: \n\tPython\n\tC\n\tJavaScript")
